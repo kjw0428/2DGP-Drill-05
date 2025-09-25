0bb4854349856e4c7bc8c2ca84ff24cc488a18b4
kjw0428 <naver.com@tukorea.ac.kr>
2025-09-25 23:46:14 +0900

    캐릭터 생성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,1 +1,19 @@
 from pico2d import *
+
+def IDLE():
+    frame = 0
+    center_x = 400
+    center_y = 300
+    running = True
+    while running:
+        clear_canvas()
+        character.clip_draw(frame * 128, 1152, 128, 128, center_x, center_y, 128, 128)
+        update_canvas()
+        frame = (frame + 1) % 6
+
+open_canvas()
+character = load_image('SamuraiSheet.png')
+
+IDLE()
+
+close_canvas()
